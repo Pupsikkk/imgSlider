bead0de19211968e27bd6551fb259049986d338f
VladSh <orativtech7@gmail.com>
2021-04-07 20:19:38 +0300

    add better event handler

--- js/script.js
+++ js/script.js
@@ -3,12 +3,21 @@ let imgArray = imgObjectToArray(document.querySelectorAll('.img-outp>img'))
 let left_button,right_button
 [left_button,right_button] = document.querySelectorAll('.button')
 
-left_button.addEventListener('click',shiftLeft)
-right_button.addEventListener('click',shiftRight)
+left_button.addEventListener('click',() => shift('left'))
+right_button.addEventListener('click',() => shift('right'))
 
-function shiftLeft(){
-    imgArray[0].parentElement.insertAdjacentHTML('beforeend',`${imgArray[0].outerHTML}`)
-    imgArray[0].remove();
+function shift(direction){
+    let index, insertSide
+    if (direction === 'left'){
+        index = 0
+        insertSide = 'beforeend'
+    }
+    if (direction === 'right'){
+        index = imgArray.length - 1
+        insertSide = 'afterbegin'
+    }
+    imgArray[index].parentElement.insertAdjacentHTML(insertSide,`${imgArray[index].outerHTML}`)
+    imgArray[index].remove();
     imgArray = imgObjectToArray(document.querySelectorAll('.img-outp>img'))
     imgArray[0].className = 'hidden'
     imgArray[1].className = 'left_picture'
@@ -19,22 +28,6 @@ function shiftLeft(){
             imgArray[i].className = 'hidden'
     }
 }
-
-
-function shiftRight(){
-    imgArray[imgArray.length - 1].parentElement.insertAdjacentHTML('afterbegin',`${imgArray[imgArray.length - 1].outerHTML}`)
-    imgArray[imgArray.length - 1].remove();
-    imgArray = imgObjectToArray(document.querySelectorAll('.img-outp>img'))
-    imgArray[0].className = 'hidden'
-    imgArray[1].className = 'left_picture'
-    imgArray[2].className = 'center_picture'
-    imgArray[3].className = 'right_picture'
-    if (imgArray.length >= 5){
-        for(let i = 4; i < imgArray.length; i++)
-            imgArray[i].className = 'hidden'
-    }
-}
-
 
 function imgObjectToArray(imgObj){
     let bufArr = []
